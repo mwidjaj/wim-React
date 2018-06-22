46f43138efa8af5469a9665320825ba32755334a
mwidjaja <matthewwidjaja@gmail.com>
2018-06-22 17:28:57 +0700

    koko is stuck

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
 const LoginPage = () => (
     <div>
         <h1>Login page</h1>
+
+        <LoginForm />
     </div>
 );
 
