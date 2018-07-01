90c08879633baed54061769fa6ce8bc737edd3e5
mwidjaja <matthewwidjaja@gmail.com>
2018-07-01 13:56:49 +0700

    authorised rote

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+const ConfrimEmailMessage = () => (
+    <Message info>
+        <Message.Header>Please, verify your email to unlock awesomeness</Message.Header>
+    </Message>
+);
+
+export default ConfrimEmailMessage;
